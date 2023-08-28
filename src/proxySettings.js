13609fe57f5699c33da40f425dd073d711e1c0a9
Tolulope Olofinjana <tolujana@gmail.com>
2023-08-28 01:37:15 +0100

    updated proxysetting

--- src/proxySettings.js
+++ src/proxySettings.js
@@ -1,8 +1,9 @@
 import axios from "axios";
-API = process.env.REACT_APP_BASEURL;
+
 export const axiosInstance = axios.create({
   // baseURL: 'https://socialmedia-site.herokuapp.com/api/',
-  baseURL: "https://facebook-clone-apizz.onrender.com",
+  // baseURL: "http://localhost:8800/api",
+  baseURL: "https://facebook-clone-apizz.onrender.com/api",
   // validateStatus: function (status) {
   //   return status >= 200 && status < 300; // default
   // },
