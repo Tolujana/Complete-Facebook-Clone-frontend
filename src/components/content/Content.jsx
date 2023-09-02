0cb755f880d77a6799860f393995b8dd66df5c1b
TOlu <tolujana@gmail.com>
2023-09-02 03:40:30 +0100

    updated post content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -19,6 +19,7 @@ function Content({ username }) {
     const sortedPost = res.data.sort((p1, p2) => {
       return new Date(p2.createdAt) - new Date(p1.createdAt);
     });
+    console.log("data and close", res.data);
     setPost((oldPost) => [...oldPost, ...sortedPost]);
   };
   console.log("see post", posts, user._id);
