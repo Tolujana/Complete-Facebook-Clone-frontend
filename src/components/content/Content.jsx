cb25496fee52ecc6b53b7a5e731656f2bc424cf6
TOlu <tolujana@gmail.com>
2023-09-03 23:00:53 +0100

    update api request

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -14,7 +14,7 @@ function Content({ username }) {
   const fetchPost = async (offset) => {
     const res = username
       ? await axiosInstance.get("/posts/profile/" + username + "?offset=" + offset)
-      : await axiosInstance.get(`posts/timeline/${user._id}` + "?offset=" + offset);
+      : await axiosInstance.get(`posts/timelinepost/${user._id}` + "?offset=" + offset);
 
     const sortedPost = res.data.sort((p1, p2) => {
       return new Date(p2.createdAt) - new Date(p1.createdAt);
